715ef946d57a761cf28a1b99d98c4ddf81a2ebe3
秦旭洋 <1446445040@qq.com>
2021-06-08 14:54:02 +0800

    docs: 文案修改

--- src/views/InitUserInfo.vue
+++ src/views/InitUserInfo.vue
@@ -1,10 +1,14 @@
 <template>
-  <van-dialog v-model:show="show" title="初始化" :beforeClose="beforeClose">
+  <van-dialog
+    v-model:show="show"
+    title="电子ID界面地址"
+    :beforeClose="beforeClose"
+  >
     <van-form ref="formRef">
       <van-field
         v-model="url"
         required
-        placeholder="请粘贴电子ID地址"
+        placeholder="请粘贴，点击右侧图标查看提示"
         right-icon="question-o"
         :rules="[{ validator: urlValidator }]"
         @click-right-icon="clickRightIcon"
@@ -32,7 +36,7 @@ export default {
     onMounted(() => { show.value = true; });
 
     function urlValidator(value) {
-      if (!value) return '请粘贴电子ID地址';
+      if (!value) return '请粘贴电子ID界面地址';
       try {
         const baseUrl = new URL(url.value);
         code = baseUrl.searchParams.get('code');
@@ -68,9 +72,12 @@ export default {
 
     function clickRightIcon() {
       Dialog.alert({
-        title: '初始化方法',
+        title: '地址获取方法',
         messageAlign: 'left',
-        message: '在南昌大学企业微信中打开电子ID界面，复制并在输入框中粘贴页面地址。初始化一次可使用5天左右，若初始化失败，可尝试退出并重新进入电子ID界面。示例：http://jc.ncu.edu.cn/?code=xxx。',
+        message: [
+          '在南昌大学企业微信中打开电子ID界面，点击右上角并在弹出的菜单中选择🔗复制链接，然后粘贴到输入框中。',
+          '初始化一次可使用5天左右，若初始化失败，可尝试退出并重新进入电子ID界面。链接示例：http://jc.ncu.edu.cn/?code=xxx',
+        ].join('\n'),
       });
     }
 
